3f21e7427f4ddfc5f20768f6026385df12045e97
ssup2 <supsup5642@gmail.com>
2024-02-10 13:58:33 +0900

    Update

--- main.tf
+++ main.tf
@@ -146,6 +146,51 @@ module "prometheus_adot_amp" {
   source = "terraform-aws-modules/managed-service-prometheus/aws"
 
   workspace_alias = format("%s-adot-amp", local.name)
+
+  rule_group_namespaces = {
+    CPU = {
+      name = "CPU"
+      data = <<-EOT
+      groups:
+      - name: CPU
+        rules:
+        - expr: sum(rate(container_cpu_usage_seconds_total{container!=""}[5m]))
+          record: cluster:cpu_usage:rate5m
+        - expr: rate(container_cpu_usage_seconds_total{container!=""}[5m])
+          record: cluster:cpu_usage_nosum:rate5m
+        - expr: avg(irate(container_cpu_usage_seconds_total{container!="POD", container!=""}[5m])) by (container,pod,namespace)
+          record: kubecost_container_cpu_usage_irate
+        - expr: sum(container_memory_working_set_bytes{container!="POD",container!=""}) by (container,pod,namespace)
+          record: kubecost_container_memory_working_set_bytes
+        - expr: sum(container_memory_working_set_bytes{container!="POD",container!=""})
+          record: kubecost_cluster_memory_working_set_bytes
+      EOT
+    }
+    Savings = {
+      name = "Savings"
+      data = <<-EOT
+      groups:
+      - name: Savings
+        rules:
+        - expr: sum(avg(kube_pod_owner{owner_kind!="DaemonSet"}) by (pod) * sum(container_cpu_allocation) by (pod))
+          record: kubecost_savings_cpu_allocation
+          labels:
+            daemonset: "false"
+        - expr: sum(avg(kube_pod_owner{owner_kind="DaemonSet"}) by (pod) * sum(container_cpu_allocation) by (pod)) / sum(kube_node_info)
+          record: kubecost_savings_cpu_allocation
+          labels:
+            daemonset: "true"
+        - expr: sum(avg(kube_pod_owner{owner_kind!="DaemonSet"}) by (pod) * sum(container_memory_allocation_bytes) by (pod))
+          record: kubecost_savings_memory_allocation_bytes
+          labels:
+            daemonset: "false"
+        - expr: sum(avg(kube_pod_owner{owner_kind="DaemonSet"}) by (pod) * sum(container_memory_allocation_bytes) by (pod)) / sum(kube_node_info)
+          record: kubecost_savings_memory_allocation_bytes
+          labels:
+            daemonset: "true"
+      EOT
+    }
+  }
 }
 
 module "prometheus_amp" {
@@ -917,8 +962,9 @@ resource "helm_release" "amp_kubecost" {
 resource "aws_prometheus_scraper" "amp_scraper" {
   source {
     eks {
-      cluster_arn = module.eks_amp.cluster_arn
-      subnet_ids  = module.vpc.private_subnets
+      cluster_arn        = module.eks_amp.cluster_arn
+      subnet_ids         = module.vpc.private_subnets
+      security_group_ids = [module.eks_amp.node_security_group_id]
     }
   }
 
@@ -1018,16 +1064,19 @@ scrape_configs:
     action: replace
     target_label: node
 - job_name: 'kubecost'
-  honor_labels: true
-  scrape_interval: 1m
+  scrape_interval: 60s
   scrape_timeout: 60s
   metrics_path: /metrics
   scheme: http
-  dns_sd_configs:
-  - names:
-    - cost-analyzer-cost-analyzer
-    type: 'A'
-    port: 9003
+  kubernetes_sd_configs:
+  - role: pod
+  relabel_configs:
+  - source_labels: [__meta_kubernetes_pod_label_app_kubernetes_io_instance]
+    action: keep
+    regex:  cost-analyzer
+  - source_labels: [__meta_kubernetes_pod_label_app_kubernetes_io_name]
+    action: keep
+    regex:  cost-analyzer
 - job_name: 'kubecost-networking'
   kubernetes_sd_configs:
   - role: pod
